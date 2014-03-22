8de2dbc94e664923940de16922470b6dc2a89b12
Andrew Newman <andrewfnewman@gmail.com>
2014-03-22 20:34:26 +1000

    Setup tables.

--- db/migrate/20140318120340_add_address.rb
+++ db/migrate/20140318120340_add_address.rb
@@ -1,8 +1,33 @@
 class AddAddress < ActiveRecord::Migration
   def change
-    t.text :street
-    t.text :city
-    t.text :postcode, limit: 2
-    t.text :state_name
+    create_table :address1 do |t|
+      t.text :street
+      t.text :city
+      t.text :postcode, limit: 2
+      t.text :state_name
+    end
+
+    create_table :address2 do |t|
+      t.belongs_to :state
+      t.text :street
+      t.text :city
+    end
+
+    create_table :state2 do |t|
+      t.text :postcode, limit: 2
+      t.text :state_name
+    end
+
+    create_table :address3 do |t|
+      t.text :street
+      t.text :city
+    end
+
+    create_table :state3 do |t|
+      t.text :postcode, limit: 2
+      t.text :state_name
+    end
+
+    create_join_table :address3, :state3
   end
 end
